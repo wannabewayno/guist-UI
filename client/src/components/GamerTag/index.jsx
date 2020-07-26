004fde2d4207400cd414fa46eb2be4f2b4cb6183
Wayne <wayne.c.griffiths@gmail.com>
2020-07-26 20:18:50 +0800

    Revert last commit

--- client/src/components/GamerTag/index.jsx
+++ client/src/components/GamerTag/index.jsx
@@ -3,26 +3,25 @@ import { gamertagStyle, numberStyle, containerStyle } from './style.js';
 import { colours } from '../../content'
 
 
-export default function Gamertag({ gamertag, rank, team }) {
+function parseRank(rank){
+    switch(rank){
+        case 1: return '1st'
+        case 2: return '2nd'
+        case 3: return '3rd'
+        default: return `${rank}th`
+    }
+}
 
-    
-    function parseRank(rank){
-        switch(rank){
-            case 1: return '1st'
-            case 2: return '2nd'
-            case 3: return '3rd'
-            default: return `${rank}th`
-        }
+function parseTeam(team){
+    const { salmon, red, blue } = colours
+    switch(team){
+        case 'terrorists': return red
+        case 'freedom': return blue
+        default: return salmon
     }
-    
-    function parseTeam(team){
-        const { salmon, red, blue } = colours
-        switch(team){
-            case 'terrorists': return red
-            case 'freedom': return blue
-            default: return salmon
-        }
-    }
+}
+
+export default function Gamertag({ gamertag, rank, team }) {
 
     return (
         <li style={containerStyle}>
