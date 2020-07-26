6fab9f4c02988733974490f4fc71c2102836c2fb
Wayne <wayne.c.griffiths@gmail.com>
2020-07-26 20:16:11 +0800

    MODIFY ParseTeam an ParseRank into the export statement of Gamertag

--- client/src/components/GamerTag/index.jsx
+++ client/src/components/GamerTag/index.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import { gamertagStyle, numberStyle, containerStyle } from './style.js';
 import { colours } from '../../content'
 
-function parseRank(rank){
-    switch(rank){
-        case 1: return '1st'
-        case 2: return '2nd'
-        case 3: return '3rd'
-        default: return `${rank}th`
-    }
-}
 
-function parseTeam(team){
-    const { salmon, red, blue } = colours
-    switch(team){
-        case 'terrorists': return red
-        case 'freedom': return blue
-        default: return salmon
-    }
-}
+export default function Gamertag({ gamertag, rank, team }) {
 
-export default function Gamertag({ gamertag, rank, team }){
+    
+    function parseRank(rank){
+        switch(rank){
+            case 1: return '1st'
+            case 2: return '2nd'
+            case 3: return '3rd'
+            default: return `${rank}th`
+        }
+    }
+    
+    function parseTeam(team){
+        const { salmon, red, blue } = colours
+        switch(team){
+            case 'terrorists': return red
+            case 'freedom': return blue
+            default: return salmon
+        }
+    }
+
     return (
         <li style={containerStyle}>
             <p style={{...numberStyle, borderColor:parseTeam(team)}}>
